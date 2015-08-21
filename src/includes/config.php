cd63849e88af45418e1e2e9bdefe31eb820081c5
Chris Keavey <chris@chriskeavey.com>
2015-08-21 16:26:31 +0100

    Removed root password from phpmyadmin

--- src/includes/config.php
+++ src/includes/config.php
@@ -2,6 +2,6 @@
   // Database Constants
   defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
   defined('DB_USER') ? null : define("DB_USER", "root");
-  defined('DB_PASS') ? null : define("DB_PASS", "&IV%3JXOfW6Z");
+  defined('DB_PASS') ? null : define("DB_PASS", "");
   defined('DB_NAME') ? null : define("DB_NAME", "btbsandbox");
 ?>
