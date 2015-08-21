bc61a2f5aa277dff9838415f97165dc199f5aab7
Chris Keavey <chris@chriskeavey.com>
2015-08-21 16:20:02 +0100

    Include erroneously ignored config.php

---
+++ src/includes/config.php
@@ -0,0 +1,7 @@
+<?php
+  // Database Constants
+  defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
+  defined('DB_USER') ? null : define("DB_USER", "root");
+  defined('DB_PASS') ? null : define("DB_PASS", "&IV%3JXOfW6Z");
+  defined('DB_NAME') ? null : define("DB_NAME", "btbsandbox");
+?>
