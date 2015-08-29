8b435dca693f96a7144b82ac56fa3ca0fc54ca01
Chris Keavey <chris@chriskeavey.com>
2015-08-29 18:36:02 +0100

    Add tighter management of session timeouts & cookie cleanup.

--- src/includes/session.php
+++ src/includes/session.php
@@ -12,9 +12,47 @@
     public $message;
 
     function __construct() {
+      $session_lifetime = 1800;
+      session_set_cookie_params($session_lifetime);
       session_start();
+      // session_regenerate_id(true);
+
+      $this->maintain_session($session_lifetime);
+      $this->regenerate_session_id($session_lifetime);
+
       $this->check_message();
       $this->check_login();
+
+      if($this->logged_in) {
+        // actions to take straight away if user logged in
+      } else {
+        // actions to take straight away if user is not logged in
+      }
+    }
+
+    private function maintain_session($session_lifetime) {
+      if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > $session_lifetime)) {
+        // last request was more than 30 minutes ago
+        // session_unset();     // unset $_SESSION variable for the run-time
+        // session_destroy();   // destroy session data in storage
+        logout();
+      } else {
+        $_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
+        // session_regenerate_id(true);
+        if (isset($_COOKIE[session_name()])) {
+          setcookie(session_name(), $_COOKIE[session_name()], time()+$session_lifetime, '/');
+        }
+      }
+    }
+
+    private function regenerate_session_id($session_lifetime) {
+      if (!isset($_SESSION['CREATED'])) {
+        $_SESSION['CREATED'] = time();
+      } else if (time() - $_SESSION['CREATED'] > $session_lifetime) {
+        // session started more than 30 minutes ago
+        session_regenerate_id(true);    // change session ID for the current session and invalidate old session ID
+        $_SESSION['CREATED'] = time();  // update creation time
+      }
     }
 
     public function is_logged_in() {
@@ -26,13 +64,20 @@
       if($user) {
         $this->user_id = $_SESSION['user_id'] = $user->id;
         $this->logged_in = true;
+        session_regenerate_id(true);
       }
     }
 
     public function logout() {
+      if (isset($_COOKIE[session_name()])) {
+        setcookie(session_name(), '', time()-42000, '/');
+      }
       unset($_SESSION['user_id']);
       unset($this->user_id);
       $this->logged_in = false;
+
+      $_SESSION = array();
+      session_destroy();
     }
 
     public function message($msg="") {
